32e7dd7e8ab1bfa7b523108f01f6c4fa201587a5
jameszm <s670238028@gmail.com>
2017-08-09 11:42:46 +0800

    q

--- tran.py
+++ tran.py
@@ -1,3 +1,35 @@
 #!/usr/bin/python
 
 from numpy import *
+
+Yarr = []
+Xarr = []
+
+data = open("./adult.onehot", "r")
+
+while True:
+    line = data.readline()
+    if not line:
+        break
+    elems = line.strip("\r\n").split(",")
+    xi = [1]*(len(elems))
+    for i in range(len(elems)-1):
+        xi[i] = int(elems[i].strip(" "))
+    Xarr.append(xi)
+
+    res = elems[len(elems)-1].strip(" ")
+    if res == "<=50K":
+        Yarr.append([0])
+    else:
+        Yarr.append([1])
+
+data.close()
+
+X = array(Xarr)
+Y = array(Yarr)
+
+A = dot(X.T, X)
+B = dot(X.T, Y)
+W = linalg.pinv(A).dot(B)
+
+print W
